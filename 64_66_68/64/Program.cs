8164f3b66125da3548f333eb7a1158e20eecd101
YaroSLAVE <tys_brembo@mail.ru>
2022-11-13 16:59:57 +0400

    New DZ

---
+++ 64_66_68/64/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задача 64: Задайте значение N. Напишите программу, которая выведет все 
+натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+int n = InputInt ("Введите положитлеьное число: ");
+int m = 1;
+if (n < 1)
+{
+    Console.WriteLine("Ввели не положительное число");
+
+}
+Console.WriteLine(NaturalNum(n,m));
+
+int NaturalNum(int n, int m)
+{
+    if (n == m)
+    return n;
+    else
+    Console.Write($"{NaturalNum(n,m + 1)}, ");
+    return m;
+}
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
---
+++ 64_66_68/66/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая 
+найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+int m = InputInt ("Введите M: ");
+int n = InputInt ("Введите N: ");
+Console.WriteLine($"Сумма натуральных элементов от {m} до {n} = {NaturalSum(m,n)}");
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+
+}
+
+int NaturalSum(int m, int n)
+{
+    if ( m==n)
+    return n;
+    return n + NaturalSum(m, n - 1);
+}
